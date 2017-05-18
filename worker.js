e2d223bdb9420a25320125e48f178e7f25df30bc
Matt Tanguay-Carel <matthieutc@gmail.com>
2017-05-18 20:21:55 +0200

    uses let and removes semi-colons

--- worker.js
+++ worker.js
@@ -1,24 +1,24 @@
 onmessage = function(e) {
-  console.log('Message received from main script');
+  console.log('Message received from main script')
 
-  var workerResult = fibonacciSequence(e.data);
+  let workerResult = fibonacciSequence(e.data)
 
-  console.log('Done calculating!');
-  console.log('Posting message back to main script');
+  console.log('Done calculating!')
+  console.log('Posting message back to main script')
 
   postMessage(workerResult);
 }
 
 function fibonacciSequence(n){
-  let sequence = [];
+  let sequence = []
   for (let i = 0; i <= n; i++){
-    sequence.push(fibonacci(i));
+    sequence.push(fibonacci(i))
   }
-  return sequence;
+  return sequence
 }
 
 function fibonacci(n) {
    return n < 1 ? 0
         : n <= 2 ? 1
-        : fibonacci(n - 1) + fibonacci(n - 2);
+        : fibonacci(n - 1) + fibonacci(n - 2)
 }
